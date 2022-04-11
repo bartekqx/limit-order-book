bdf0b55545d269f7adf20f91f5e087d9a247042e
bartekqx <bartlomiej.sulima7@gmail.com>
2022-04-11 19:49:25 +0200

    clean version...

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { SignInService } from './services/sign-in.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    AppRoutingModule,
+    RouterModule
+  ],
+  providers: [SignInService],
+  bootstrap: [AppComponent],
+  exports: [AppRoutingModule]
+})
+export class AppModule { }
